f050db31278da4f5c2c48f819e4b0d44a28036ac
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2020-03-10 07:35:11 +0100

    .gitignore fix to untrack log folder

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const {logger} = require('./loggers/logger')
-require('dotenv').config();
-
-app.listen(process.env.PORT, ()=>{
-    logger.info(`Application is running on port ${process.env.PORT}`)
-})
